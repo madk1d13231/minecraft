397618d2055b65a7f9b639abb2d87311b25eb046
a1oha <dimashsamal52@gmail.com>
2026-08-20 11:47:28 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
+const auth = getAuth();
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Успешный вход
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    if (errorCode === 'auth/invalid-credential') {
+      alert("Неверный email или пароль. Пожалуйста, проверьте введенные данные.");
+    } else {
+      alert("Ошибка входа: " + error.message);
+    }
+  });
